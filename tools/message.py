99ed735c59b2fc820455b29d1a9491dd6c5c8c64
abelbcarvalho <abelbdecarvalho@gmail.com>
2021-04-06 19:28:11 -0300

    comentario adicionado em Message

--- tools/message.py
+++ tools/message.py
@@ -1,6 +1,15 @@
 class Message:
+    """Classe que recebe mensagens que 
+    serão exibidas em algum dos métodos do modulo
+    messagebox.
+
+    Author:
+        bcarsoft
+    """
 
     def __init__(self):
+        """Construtor.
+        """
         self._msg = ''
 
     @property
